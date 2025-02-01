28397a93329314ce210c173ac2eadce26ea7ab10
Yose Rafhael <yrafhael@my.wctc.edu>
2025-01-31 23:34:05 -0600

    [1.0] Initial commit

---
+++ discount.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('discount-row').addEventListener('click', function (e) {
+        if (e.target.classList.contains('discount')) {
+            e.preventDefault();
+            document.getElementById('product').innerHTML = e.target.dataset['product'];
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+          }
+    });
+});
